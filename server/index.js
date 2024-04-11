78b85e928f87753d4b20a537537769faee4447c1
Nagender211 <dnagender2019@gmail.com>
2024-04-11 21:09:22 +0530

    added the files

--- server/index.js
+++ server/index.js
@@ -1,24 +1,21 @@
-import express from "express";
-import * as dotenv from "dotenv";
 import cors from "cors";
+import express from "express";
 import mongoose from "mongoose";
-import generateImageRoute from "./routes/GenerateImage.js";
-import posts from "./routes/Posts.js";
+import * as dotenv from "dotenv";
+import PostRouter from "./routes/Posts.js";
+import GenerateImageRouter from "./routes/GenerateImage.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true })); // for form data
-
-app.use("/api/generateImage/", generateImageRoute);
-app.use("/api/post/", posts);
+app.use(express.urlencoded({ extended: true }));
 
 // error handler
 app.use((err, req, res, next) => {
   const status = err.status || 500;
-  const message = err.message || "Something went wrong";
+  const message = err.message || "Something went wrong!";
   return res.status(status).json({
     success: false,
     status,
@@ -26,23 +23,29 @@ app.use((err, req, res, next) => {
   });
 });
 
+app.use("/api/post", PostRouter);
+app.use("/api/generateImage", GenerateImageRouter);
+
+//Default get
 app.get("/", async (req, res) => {
   res.status(200).json({
-    message: "Wel come to nagAi",
+    message: "Hello GFG Developers!",
   });
 });
 
+//function to connect to mongodb
 const connectDB = () => {
   mongoose.set("strictQuery", true);
   mongoose
     .connect(process.env.MONGODB_URL)
-    .then(() => console.log("Connected to Mongo DB"))
+    .then(() => console.log("MongoDB Connected"))
     .catch((err) => {
-      console.error("failed to connect with mongo");
+      console.error("Failed to connect to DB");
       console.error(err);
     });
 };
 
+//function to start the server
 const startServer = async () => {
   try {
     connectDB();
